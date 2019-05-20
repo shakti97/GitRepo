14e4b9eeb2c1ff6536be0131fb1ff2dea5cee02d
shakti97 <shaktisingh230797@gmail.com>
2019-05-20 15:48:17 +0530

    Refactor App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RepoListService } from './Services/repo-list.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'GitRepo';
+  userName : string='';
+  repoList : any[];
+  myRepoList : any[]=[];
+  homeScreen : boolean=true;
+
+  constructor(private reposervice : RepoListService) { }
+  
+  onUserNameChanged(username : string):void{
+    this.userName=username;
+    console.log('AppComponent ',this.userName);
+    this.getRepoList(this.userName);
+  }
+  onAddToMyRepo(repoId:string):void{
+    this.myRepoList.push(...this.repoList.filter(repo=>repo.id==repoId));
+    console.log(this.myRepoList);
+  }
+  getRepoList(userName:string){
+    this.reposervice.getRepos(userName).subscribe((data: any[])=> this.repoList=data)
+  }
+  toggleScreen(){
+    console.log('toggleScreen');
+    this.homeScreen=!this.homeScreen;
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchUserComponent } from './Components/search-user/search-user.component';
+import { RepoListComponent } from './Components/repo-list/repo-list.component';
+import { PickedRepoComponent } from './Components/picked-repo/picked-repo.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RepoListService } from './Services/repo-list.service';
+import { HeaderComponent } from './Components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchUserComponent,
+    RepoListComponent,
+    PickedRepoComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [RepoListService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
